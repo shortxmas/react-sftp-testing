a3f79ec3f5a4480fb0f0ae556a303ea8bbad458f
Leighton Miguel <leim56@hawaii.rr.com>
2024-03-23 01:59:20 -1000

    r

--- src/components/ExampleComponent.tsx
+++ src/components/ExampleComponent.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+import { FC } from "react";
 
-const ExampleComponent = () => {
+const ExampleComponent:FC = () => {
   return (
     <>
       <div>This is an example component.</div>
